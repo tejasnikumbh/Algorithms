18d8daa19ede5e029a65ddb842e3418db2ffb182
Tejas Nikumbh <tejasnikumbh@gmail.com>
2014-12-04 01:48:51 +0530

    Template added

---
+++ Warmup/Template.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Instance Variale to parse in data
+    private static BufferedReader dataStream;
+
+    public static void main(String[] args) throws Exception{
+        
+        // Parsing the input from the data stream
+        dataStream  =  new BufferedReader(new InputStreamReader(System.in));
+        
+        //Creating a new instance of the problem
+        Solution soln = new Solution();
+        
+        // Printing Output
+        
+    }
+    
+}
