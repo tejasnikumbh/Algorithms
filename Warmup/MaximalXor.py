152e1b7956bb2aec276e3cd01ebd6b878877a8b6
Tejas Nikumbh <tejasnikumbh@gmail.com>
2014-12-06 21:59:27 +0530

    SherlockAndBeast

--- Warmup/MaximalXor.py
+++ Warmup/MaximalXor.py
@@ -4,17 +4,20 @@
 '''
     Function that computes maximal or of digits in a range
     Logic : firstly, take Xor of least and most elements in range
-    Two Cases - Same MSB, Different MSB [in their binary representations]
-    If same MSB , then all elements in range have same MSB (since range is l to r)
-    So maximal Xor CANNOT have MSB as 1 (since any pair will have same MSB as 1 and
-    hence will give a Xor with MSB of 0). 
-    If different MSB then Max MSB CAN have 1 as MSB and it MUST since if it does not 
-    then simply take Xor of least and most elements. Similar Logic for (MSB +1)th digit
-    and so on.
+    If same MSB , then Xor MSB will be 0 .Similar logic upto MSB 1 in Xor
+    When MSB is 1 in Xor, implies least and most range differ in atleast that
+    position, least having 0 and most having 1. Even if we include the least
+    value of such a range, lets say Xor is 00101, then the l = 00011 and r = 00100
+    This is where the flip occurs from 11 to 100 and Xor is maximized. 111. Whose
+    value is 2^3 - 1. 
+    
     So answer is simply 2^ position of MSB from right(indexing as 1) - 1
+    
+    Take care of edge case when l = r, in which case return 0
 '''
 
 def maxXor(l,r):
+    if( l == r): return 0
     a = l^r
     strLen = len(str(bin(a)[2:]))
     return 2 ** (strLen) - 1
