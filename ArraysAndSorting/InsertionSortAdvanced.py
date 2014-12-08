8a37caaa932c2ec7b7efd42b303f1c4c63660cee
Tejas Nikumbh <tejasnikumbh@gmail.com>
2014-12-08 06:11:39 +0530

    Mergesort Inversion Count

---
+++ ArraysAndSorting/InsertionSortAdvanced.py
@@ -0,0 +1,91 @@
+# Importing standard libraries
+import sys
+
+# ------------------------------------------------------------------- #
+# Important things to note in program about the nature of Pythons pass#
+# by reference. The array a to be  sorted is a list and is a mutable  #
+# type. There fore, when it is passed as a parameter, a reference to  #
+# that list object is passed . YOU SHOULD NOT REASSIGN this reference #
+# if you want the same a to be altered by the mergesort algorithm in  #
+# recursive calls of the sort procedure as well as the merge procedure#
+# Note :                                                              #
+# This version of mergesort takes an auxilary space of O(N) and is    #
+# also good from point of view of brevity                             #
+# Reference for Python variable passing Mechanisms:                   #
+# http://stackoverflow.com/questions/4344017/how-can-i-get-the-       #
+# concatenation-of-two-lists-in-python-without-modifying-either       #
+# First Answer with highest upvotes                                   #
+# ------------------------------------------------------------------- #
+
+'''
+    Function that counts the number of inversions in the array using
+    Mergesort .  This reduces the time complexity to count the no of
+    inversions and does it in O(NlogN) time
+'''
+def countInversions(a):
+    inversions = sort(a,0,len(a)-1)
+    return inversions
+
+
+'''
+    Sort Function of mergeSort. According to the recursive substructure
+    THe number of inversions is simply inversions in left subarray sort
+    + inversions in right subarray sort + inversions done while merging
+'''
+def sort(a,start,end):
+    if(start >= end): return 0
+    else:
+        midPoint = (start + end) / 2
+        inversionsLeft = sort(a,start,midPoint)
+        inversionsRight = sort(a,midPoint+1,end)
+        inversions = merge(a,start,midPoint,midPoint + 1,end)
+        return inversions + inversionsLeft + inversionsRight
+    
+'''
+    Merge function of mergesort in python. The number of inversions
+    are incremented here. They are computed by the following logic.
+    If an element from right subarray is being merged into the 
+    main array a in sorted order, then the number of inversions 
+    for each such element being merged from the right subarray
+    should be incremented by the elements left in the left subarray
+'''
+def merge(a,start1,end1,start2,end2):
+    merged = [0]*(end2 - start1 + 1)
+    count = 0
+    inversions = 0
+    i = start1
+    j = start2
+    while(i<=end1 and j<=end2):
+        if(a[i]<=a[j]):
+            merged[count] = a[i]
+            count += 1
+            i += 1
+        else: #Inversions incremented here
+            inversions += (end1 - i + 1)
+            merged[count] = a[j]
+            count += 1
+            j += 1
+    while(i<=end1): 
+        merged[count] = a[i]
+        count += 1
+        i += 1
+    while(j<=end2): 
+        merged[count] = a[j]
+        count += 1
+        j += 1
+    for i in range(start1,end2+1):
+        a[i] = merged[i - start1]
+    return inversions
+        
+'''
+    Main function for the program. Simple Inversion
+    counting can be done using insertion sort itself but using
+    MergeSort is an efficient method
+'''
+if __name__ == "__main__":
+    t = int(sys.stdin.readline().rstrip())
+    for i in range(t):
+        n = int(sys.stdin.readline().rstrip())
+        a = [int(x) for x in sys.stdin.readline().rstrip().split()]
+        inversions = countInversions(a)
+        print inversions
---
+++ HackerCup/SquareDetector.py
@@ -0,0 +1,43 @@
+# Importing standard libraries
+import sys
+
+'''
+	Function that returns a list of indices in the grid which have
+	the specific character supplied as a parameter
+'''
+def getIndices(grid,character):
+    indices = []
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if(grid[i][j] == '#'):
+                indices.append([i,j])
+    return indices
+    
+if __name__ == "__main__":
+    f = open('square_detector_example_input.txt')
+    t = int(f.readline().rstrip())
+    for i in range(t):
+        n = int(f.readline().rstrip())
+        grid = []
+        for j in range(n):
+            curRow = list(f.readline().rstrip())
+            grid.append(curRow)
+        filledCells = getIndices(grid,"#")    
+        rowIndices = [item[0] for item in filledCells]
+        colIndices = [item[1] for item in filledCells]
+        minRow = min(rowIndices)
+        maxRow = max(rowIndices)
+        minCol = min(colIndices)
+        maxCol = max(colIndices)
+		# Checking if it is a complete rectangle
+        isSquare = True
+        for j in range(minRow,maxRow + 1):
+            for k in range(minCol,maxCol + 1):
+                if(grid[j][k] != '#'): isSquare = False
+		# Checking if sides of the rectangle are equal		
+		if(maxRow - minRow != maxCol - minCol):
+			isSquare = False
+        if(isSquare):
+            print "Case #" + str(i+1) + ": YES"
+        else:
+            print "Case #" + str(i+1) + ": NO"
