a8f2390350e67c338c18361a2f4803ede5a87388
Tejas Nikumbh <10d070019@iitb.ac.in>
2014-12-12 17:56:41 +0530

    Delete printArray.py

--- genericCode/printingFunctions/printArray.py
+++
@@ -1,9 +0,0 @@
-'''
-    Function to print the array with numbers in a space seperated format
-'''
-def printArray(a,delimiter):
-    arrayStr = ""
-    for i in a:
-         arrayStr += str(i) + delimiter
-    arrayStr.rstrip()
-    print arrayStr
