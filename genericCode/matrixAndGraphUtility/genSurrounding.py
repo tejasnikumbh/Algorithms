f00ad1bde47c796f0a4fc0b883231f8ecc1e0bcb
Tejas Nikumbh <10d070019@iitb.ac.in>
2014-12-12 17:46:43 +0530

    Update genSurrounding.py

--- genericCode/matrixAndGraphUtility/genSurrounding.py
+++ genericCode/matrixAndGraphUtility/genSurrounding.py
@@ -2,18 +2,23 @@
 '''
     Function that generates the valid surrounding indices for a particular index in a grid
     The surroundings are just 4 as of now. But this function can easily be modified by
-    modifying the surroundingIndices array
+    modifying the surrIndices array
 '''
-def genSurrounding(grid,i,j):
+def genSurr(grid,i,j):
     validIndices = []
-    surrIndices = [ [1,0] , [-1,0] , [0,1] , [0,-1] ]
+    surrIndices = [ (1,0) , (-1,0) , (0,1) , (0,-1) ]
     if(len(grid) == 0): return -1
     else:
         # Number of rows and columns in grid
         ROWS = len(grid)
         COLS = len(grid[0])
-        for [a,b] in surrIndices:
-            if((i + a) < 0 or (i + a) >= COLS or (j + b) < 0 or (j + b) >= ROWS): continue
-            else: validIndices.append([i + a,j + b])
+        for (a,b) in surrIndices:
+            xIndex = i + a
+            yIndex = j + b
+            if(xIndex >= ROWS or xIndex < 0):
+                continue
+            if(yIndex >= COLS or yIndex < 0):
+                continue
+            validIndices.append((xIndex,yIndex))
     return validIndices
 
