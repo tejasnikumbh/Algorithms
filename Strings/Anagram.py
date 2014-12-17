752aaaad2c6ced3c67474c805787958fcb4933f3
Tejas Nikumbh <10d070019@iitb.ac.in>
2014-12-17 17:23:47 +0530

    Update Anagram.py

--- Strings/Anagram.py
+++ Strings/Anagram.py
@@ -1,11 +1,25 @@
+# Importing standard libraries
 import sys
 
+'''
+    Parsing method that reads and parses and integer from the stream
+    passed in as parameter.
+'''
 def parseInt(s):
     return int(s.readline().rstrip())
 
+'''
+    Parsing method that reads and parses a string from the stream that
+    is passed in as parameter.
+'''
 def parseString(s):
     return s.readline().rstrip()
 
+
+'''
+    Method that returns number of edits required to convert string to its
+    corresponding valid anagram
+'''
 def makeAnagram(s):
     s1 = s[:len(s)/2]
     s2 = s[len(s)/2:]
@@ -16,6 +30,9 @@ def makeAnagram(s):
         fs1[ord(s2[i]) - ord('a')] -= 1
     return sum([x for x in fs1 if x > 0])
 
+'''
+    Main function for the program
+'''
 if __name__ == "__main__":
     stream = sys.stdin
     t = parseInt(stream)
