b88513335b55b31642c9b9569286daa57450e9ee
Tejas Nikumbh <10d070019@iitb.ac.in>
2014-12-19 19:53:52 +0530

    Update parsingFunctions.py

--- genericCode/printingAndParsingFunctions/parsingFunctions.py
+++ genericCode/printingAndParsingFunctions/parsingFunctions.py
@@ -1,4 +1,13 @@
 '''
+    Usage: TO read from files pass in the file stream as follows -  
+    E.g -> f = open('filename.txt')
+    parseInt(f)     
+    To read from standard input stream, use these functions as follows
+    E.g -> s = sys.stdin
+    parseInt(s)
+'''
+
+'''
     Parses a grid from the passed in stream. Can be used to parse the 
     grid from standard input (by passing in sys.stdin) as well as from
     a text file (by passing in f, where f = open('somename.txt'))
@@ -24,3 +33,16 @@ def parseInt(stream):
 def parseIntArr(stream):
     return [int(x) for x in stream.readline().rstrip().split()]
  
+'''
+    Reads in a string from stream passed in as the parameter. Simple
+    parsing function that can read from files as well as standard input
+'''
+def parseString(stream):
+    return stream.readline().rstrip()
+    
+'''
+    Reads String array from stream passed in as parameter. Simple parse
+    function that can raed from files as well as standard input
+'''
+def parseStringArr(stream):
+    return [str(x) for x in stream.readline().rstrip().split()]
