3de3044aaf41264ca2778c15c342cf8c7a246d1f
Tejas Nikumbh <10d070019@iitb.ac.in>
2014-12-19 16:00:12 +0530

    Update RandomNumberGenerator.py

--- Probability/RandomNumberGenerator.py
+++ Probability/RandomNumberGenerator.py
@@ -1,14 +1,25 @@
+# Importing standard libraries
 import sys
 import fractions
-from math import ceil
-from math import floor
 
+'''
+    Parsing function that parses an int from given stream
+'''
 def parseInt(s):
     return int(s.readline().rstrip())
 
+'''
+    Parsing function that parses an integer array from given stream
+'''
 def parseIntArr(s):
     return [int(x) for x in s.readline().rstrip().split()]
 
+'''
+    First we find the probability distribution of a sum of two uniform
+    random variables. This is done by convoluting their respective pdfs
+    Once this is done, value of P(x+y < C) is computed using the cdf of 
+    the function and output is returned in Fractional form.
+'''
 def getProb(a,b,c):
     ta = min(a,b)
     tb = max(a,b)
@@ -31,6 +42,9 @@ def getProb(a,b,c):
        
     return fractions.Fraction(num,denom)    
 
+'''
+    Main function to run the program
+'''
 if __name__ == "__main__":
     stream = sys.stdin
     t = parseInt(stream)
