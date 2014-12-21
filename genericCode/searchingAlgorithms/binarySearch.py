db9dc64cad657f6e6831bc3f7ad7ba02ed15d1cf
Tejas Nikumbh <10d070019@iitb.ac.in>
2014-12-21 17:21:11 +0530

    Update binarySearch.py

--- genericCode/searchingAlgorithms/binarySearch.py
+++ genericCode/searchingAlgorithms/binarySearch.py
@@ -1,15 +1,16 @@
+
 '''
     Utility method that does a binarySearch on the sorted array
     a to return the index of the element i in the array. Searches
     the array a in the range (start,end) - both inclusive
     Time Complexity : log(N)
     Returns the Index of the element if found
-    Returns -1 other wise
+    Returns index of immdiately greater element otherwise
 '''
 def binarySearch(a,start,end,i):
     # Base case if element not found
     if(start > end): # Equal to step will hold in case size = 1
-        return -1
+        return start
     # Current step for the binary Search
     startElem = a[start]
     endElem = a[end]
@@ -27,4 +28,3 @@ def binarySearch(a,start,end,i):
         return binarySearch(a,start,midIndex - 1,i)
     elif(i > midElem): # If i lies in right range
         return binarySearch(a,midIndex + 1,end,i)
-
