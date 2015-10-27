7d17d22d84c677d0d6eabec930196fa1d53a2d19
Tejas Nikumbh <10d070019@iitb.ac.in>
2015-10-28 03:10:26 +0530

    Create regexUtils.py

---
+++ genericCode/stringAlgos/regexUtils.py
@@ -0,0 +1,26 @@
+# Utilities for regex
+
+import re
+
+'''
+  Explanation : 
+  Finds indices of all occurences of a pattern p inside a string. The pattern is passed as 
+  a string and not as a compiled regex. 
+  Returns : The list of starting indices of all occurences of pattern, non Overlapping
+'''
+def findallNonOverlapping(pattern, searchString):
+  pattern = re.compile(pattern)
+  return [x.start() for x in pattern.findall(searchString)]
+  
+'''
+  Explanation : 
+  Finds indices of all occurences of a pattern p inside a string. The pattern is passed as 
+  a string and not as a compiled regex. 
+  Returns : The list of starting indices of all occurences of pattern, Overlapping
+'''
+def findallOverlapping(pattern, searchString):
+  pattern = "(?=" + pattern + ")"
+  pattern = re.compile(pattern)
+  return [x.start() for x in pattern.finditer(searchString)]
+
+
